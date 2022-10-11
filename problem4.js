3c63390a26d49727b476c5604bde237be735bed3
prnagariyanka <115461253+prnagariyanka@users.noreply.github.com>
2022-10-11 18:35:34 +0530

    i'm updating for files here

---
+++ problem4.js
@@ -0,0 +1,15 @@
+let  database_user="prnagar96@gmail.com";
+let database_pass="123abc";
+let user="prnagar96@gmail.com";
+  let pass="123abc";
+if(database_user==user){
+  if(database_pass==pass){
+    console.log("Login Successful");
+  }else{
+    console.log("incorrect password");
+  }
+
+}else {
+  console.log("wrong credentials");
+
+}
